40fe1e67ff9c8296a002e77c39700aecf8133fc2
Victor <victormanhando32@gmail.com>
2022-06-06 18:37:42 +0100

    Fixed input

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import json
 import requests
+from datetime import datetime
 
 class App(tk.Tk):
     def __init__(self):
@@ -14,26 +15,37 @@ class App(tk.Tk):
         self.title("Weather")
         self.geometry("400x400")
         self.resizable(False, False)
-    
+  
     def create_widgets(self):
         self.heading_label = tk.Label(self, 
                                 text="Weather", 
                                 font=("Sans Serif", 20),
-                                fg="dark blue",)
+                                fg="dark blue")
         
-
+        self.input_label = tk.Label(self,
+                                    text = "Enter a location:"
+                                    )
         self.location = tk.Entry(self, width=30)
         self.enter_button = tk.Button(self, text="Enter", command=self.get_weather)
+        self.reset_button = tk.Button(self, text="Reset", command=self.clear)
         
     
     def display_widgets(self):
         self.heading_label.pack(pady=3, padx=3)
+        self.input_label.pack(pady=3, padx=3)
         self.location.pack(pady=3, padx=3)
         self.enter_button.pack(pady=3, padx=3)
+        self.reset_button.pack(pady=3, padx=3)
        
 
-    
+    def clear(self):
+        self.location.delete(0, tk.END)
+        self.results_label.destroy()
+        self.enter_button.config(state="normal")
+        
+
     def get_weather(self): 
+        self.enter_button.config(state="disabled")
         with open("settings.json", "r") as jsonfile:
             data = json.load(jsonfile)
             weather_api_key = data["weather_api_key"]
@@ -41,6 +53,7 @@ class App(tk.Tk):
             url = f'{base_url}?q={self.location.get()}&appid={weather_api_key}&units=metric'
             with requests.get(url) as resp:
                 r = resp.json()
+            
         if r["cod"] != "404":
             self.current_temperature = r["main"]["temp"]
             self.current_pressure = r["main"]["pressure"]
@@ -52,8 +65,22 @@ class App(tk.Tk):
                     f"Temperature: {self.current_temperature}°C" + "\n" +
                     f"Humidity: {self.current_humidity}%" + "\n" +
                     f"Atmospheric Pressure: {self.current_pressure}hPa" + "\n" +
-                    f"Description: {self.weather_description}")
-            self.results_label.pack(pady=3, padx=3)
+                    f"Description: {self.weather_description}"+ "\n" +
+                    f"Time: {datetime.now().strftime('%H:%M:%S')}")
+        elif r["cod"] == "404":
+            self.results_label = tk.Label(self,
+                    text="Location not found, please try again")
+        else:
+            self.results_label = tk.Label(self,
+                    text="An error occured")
+        self.results_label.pack(pady=3, padx=3)
+        self.thanks_label = tk.Label(self,
+                text = f"Thanks for Using WeatherPy")
+        self.thanks_label.pack(pady=3, padx=3)
 
-application = App()
-application.mainloop()
+def main():
+    application = App()
+    application.mainloop()
+
+if __name__ == "__main__":
+    main()
